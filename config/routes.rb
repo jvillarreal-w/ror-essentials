44477674f74017ea548b957999a481046ca0440e
Jaime Villarreal <jaime.villarreal@wizeline.com>
2021-11-09 16:58:23 -0600

    Added CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
-  
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
+
+
   root 'demo#index'
   
   get 'demo/index'
@@ -17,6 +8,29 @@ Rails.application.routes.draw do
   get 'demo/about'
   get 'demo/contact'
 
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+  
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
   # default route
   #get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
